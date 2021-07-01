7cb32505e1e4f9a2a9e2fcdc86b1f5dc677fc194
renatomak <renato.mark.silva@gmail.com>
2021-06-30 23:18:43 -0300

    calculo de perimetro

---
+++ semana_02/lista_exercicio/quadrado.py
@@ -0,0 +1,5 @@
+side_square = input("Digite o valor correspondente ao lado de um square: ")
+side_square_int = int(side_square)
+perimeter_square = side_square_int * 4
+area = side_square_int ** 2
+print("perímetro:", perimeter_square, "- área:", area)
