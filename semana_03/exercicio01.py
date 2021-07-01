ea9afc87a24eef934399a6652593c3f4500c1aec
renatomak <renato.mark.silva@gmail.com>
2021-06-30 23:19:36 -0300

    exemplos do comteúdo

---
+++ semana_03/exercicio01.py
@@ -0,0 +1,13 @@
+x = 10
+y = 15
+z = 25
+print(x == z - y and z != y - x or not y != z - x)
+
+y = 20
+z = 30
+
+print(not y > 10 or not z > 10)
+
+print(x >= 10 or y != z - x)
+print(x <= 30 and y >= 5)
+print(not y < 10 or not z == 10)
