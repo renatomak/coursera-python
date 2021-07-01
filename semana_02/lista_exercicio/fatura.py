519d84bb89d59b2ca6fbfdb5042e07b1d17d63b8
renatomak <renato.mark.silva@gmail.com>
2021-06-30 23:18:15 -0300

    calculo da fatura

---
+++ semana_02/lista_exercicio/fatura.py
@@ -0,0 +1,10 @@
+name = input("Digite o nome do cliente: ")
+expiration_day = input("Digite o dia de vencimento: ")
+expiration_month = input("Digite o mês de vencimento: ")
+invouce_amount = input("Digite o valor da fatura: ")
+
+print("Olá,", name)
+print("A sua fatura com vencimento em", 
+expiration_day, "de", 
+expiration_month, "no valor de R$",
+invouce_amount, "está fechada.")
