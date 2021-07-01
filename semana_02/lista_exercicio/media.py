006ab1ec8092a60ec5d56a1d111fc1a6b478c0f0
renatomak <renato.mark.silva@gmail.com>
2021-06-30 23:18:32 -0300

    calculo da media

---
+++ semana_02/lista_exercicio/media.py
@@ -0,0 +1,8 @@
+note1 = input("Digite a primeira nota: ")
+note2 = input("Digite a segunda nota: ")
+note3 = input("Digite a terceira nota: ")
+note4 = input("Digite a quarta nota: ")
+
+media = (float(note1) + float(note2) + float(note3) + float(note4))/4
+
+print("A média aritmética é", media)
