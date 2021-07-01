83fb27d9dd555a5c35ece61a86069576209db473
renatomak <renato.mark.silva@gmail.com>
2021-06-30 23:21:12 -0300

    exemplo de contúdo

---
+++ semana_04/exemplo1.py
@@ -0,0 +1,19 @@
+""" x = 1
+while (x < 4):
+    x = x + 1
+print("x vale", x)
+
+i = 0
+while (i < 5):
+    print(i)
+    i = i + 1 """
+
+i = 2
+while i % 3 != 0:
+    print(i)
+    i += 2
+
+i = 6
+while (i > 0):
+    i = i - 3
+    print(i)
