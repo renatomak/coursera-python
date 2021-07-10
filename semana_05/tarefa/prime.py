76295dd71a339c15453b59dc0b86385e6b3fc45a
renatomak <renato.mark.silva@gmail.com>
2021-07-10 00:11:27 -0300

    funçao que encontra o maior numero primo

--- semana_05/tarefa/prime.py
+++ semana_05/tarefa/prime.py
@@ -1,2 +1,16 @@
 def prime(num):
-  
+  for dividend in range(2, num):
+    if num % dividend == 0:
+      return False
+  return True
+
+# print(prime(9))
+
+
+def find_the_largest_prime_number(num):
+  for valor in reversed(range(2, num+1)):
+    if prime(valor):
+      return valor
+
+
+# print(find_the_largest_prime_number(100))
