6ac661ea4be127da18b560c60fefa5d2a15e34c0
renatomak <renato.mark.silva@gmail.com>
2021-07-11 16:30:49 -0300

    tarefa bonus função fizz_buzz

---
+++ semana_05/tarefa/fizz_buzz.py
@@ -0,0 +1,13 @@
+def fizz_buzz(number):
+  is_divisible_by_three = number % 3 == 0
+  is_divisible_by_five = number % 5 == 0
+
+  if (is_divisible_by_three and is_divisible_by_five):
+    return 'FizzBuzz'
+  else:
+    if is_divisible_by_three:
+      return 'Fizz'
+    elif is_divisible_by_five:
+      return 'Buzz'
+    else:
+      return number
