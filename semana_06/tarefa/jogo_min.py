db1def09f0984c900c3efbc57a7e3ad6b917c4dc
renatomak <renato.mark.silva@gmail.com>
2021-07-13 20:44:30 -0300

    jogo do nim finalizado

--- semana_06/tarefa/jogo_min.py
+++ semana_06/tarefa/jogo_min.py
@@ -1,3 +1,8 @@
+MSG_INIT = """Bem-vindo ao jogo do NIM! Escolha:
+
+1 - para jogar uma partida isolada
+2 - para jogar um campeonato 2"""
+
 def find_all_multiple(n):
   return [ multiple for multiple in range(1, n+1) if n % multiple == 0]
 
@@ -6,65 +11,71 @@ def check_multiple(n, m):
   return True if m in list_multiple else False
 
 
-# check_multiple(16, 3)
+def pc_game(n, m):
+  game = n % (m+1)
+  if game == 0:
+    game = m
+  
+  print(f'\nO computador tirou {game} peça.')
 
-""" Bem-vindo ao jogo do NIM! Escolha:
+  return n-game
 
-1 - para jogar uma partida isolada
-2 - para jogar um campeonato 2
+def user_game(n, m):
+  game = 0
 
-Voce escolheu um campeonato!
+  while game == 0:
+    game = int(input('\nQuantas peças você vai tirar? '))
+    if game <= 0 or game > m:
+      print('\nOops! Jogada inválida! Tente de novo.')
+      game = 0
 
-**** Rodada 1 ****
+  print(f'Você tirou {game} peça.')
+  return n-game
 
-Quantas peças? 3
-Limite de peças por jogada? 1
 
-Computador começa!
+def game_round(count):
+  print(f'**** Rodada {count} ****')
+  n = int(input('Quantas peças? '))
+  m = int(input('Limite de peças por jogada? '))
+  
+  pc_start = check_multiple(n, m)
 
-O computador tirou uma peça.
-Agora restam 2 peças no tabuleiro.
+  if pc_start:
+    print('\nComputador começa!')
+  else:
+    print('\nVoce começa!')
+  
+  play_now = pc_start
 
-Quantas peças você vai tirar? 2
+  while n > 0:
+    if play_now:
+      n = pc_game(n, m)
+      play_now = False
+    else:
+      n = user_game(n, m)
+      play_now = True
+    
+    if n == 0:
+      print('Fim do jogo! O computador ganhou!')
+    else:
+      print(f'Agora restam {n} peças no tabuleiro.')
 
-Oops! Jogada inválida! Tente de novo.
 
-Quantas peças você vai tirar? 1
 
-Você tirou uma peça.
-Agora resta apenas uma peça no tabuleiro.
+def game_nim():
+  print(MSG_INIT)
+  choice = int(input())
+  if choice == 1:
+    print('Você escolheu uma partida isolada')
+    game_round(1)
+  elif choice == 2:
+    count = 1
+    print('Voce escolheu um campeonato!')
+    while count <= 3:
+      game_round(count)
+      count += 1
+    print('\n**** Final do campeonato! ****')
+    print('\nPlacar: Você 0 X 3 Computador')
 
-O computador tirou uma peça.
-Fim do jogo! O computador ganhou!
 
-**** Rodada 2 ****
-
-Quantas peças? 3
-Limite de peças por jogada? 2
-
-Voce começa!
-
-Quantas peças você vai tirar? 2 
-Voce tirou 2 peças.
-Agora resta apenas uma peça no tabuleiro.
-
-O computador tirou uma peça.
-Fim do jogo! O computador ganhou!
-
-**** Rodada 3 ****
-
-Quantas peças? 4
-Limite de peças por jogada? 3
-
-Voce começa!
-
-Quantas peças você vai tirar? 2
-Voce tirou 2 peças.
-Agora restam 2 peças no tabuleiro.
-
-O computador tirou 2 peças.
-Fim do jogo! O computador ganhou!
-
-**** Final do campeonato! ****
-
-Placar: Você 0 X 3 Computador """
+game_nim()
