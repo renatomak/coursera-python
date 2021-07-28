be52c43a149b012e923e38c6857550d62d2a9e44
renatomak <renato.mark.silva@gmail.com>
2021-07-28 00:56:18 -0300

    refatorado o exercicio

--- semana_07/tarefa_de_programacao/imprime_retangulo_vazando.py
+++ semana_07/tarefa_de_programacao/imprime_retangulo_vazando.py
@@ -1,13 +1,13 @@
-altura_retangulo = int(input())
-largura_retangulo = int(input())
+altura_retangulo = int(input('digite a largura: '))
+largura_retangulo = int(input('digite a altura: '))
 
-for altura in range(altura_retangulo):
-    for largura in range(largura_retangulo):
-        if altura == 0 or altura == altura_retangulo -1:
+for largura in range(largura_retangulo):
+    for altura in range(altura_retangulo):
+        if altura == 0 or altura == altura_retangulo - 1:
             print("#", end='')
         else:
-            if largura == 0 or largura == largura_retangulo -1:
+            if largura == 0 or largura == largura_retangulo - 1:
                 print("#", end='')
-            else: 
+            else:
                 print(' ', end='')
     print()
