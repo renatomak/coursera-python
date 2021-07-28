17193336de3f93642ad6fde4525f0d1f3fcefbed
renatomak <renato.mark.silva@gmail.com>
2021-07-28 00:55:54 -0300

    finalizado o jogo do nim

--- semana_06/tarefa/jogo_min.py
+++ semana_06/tarefa/jogo_min.py
@@ -1,7 +1,7 @@
 MSG_INIT = """Bem-vindo ao jogo do NIM! Escolha:
 
 1 - para jogar uma partida isolada
-2 - para jogar um campeonato 2"""
+2 - para jogar um campeonato """
 
 def find_all_multiple(n):
   return [ multiple for multiple in range(1, n+1) if n % multiple == 0]
@@ -11,34 +11,34 @@ def check_multiple(n, m):
   return True if m in list_multiple else False
 
 
-def pc_game(n, m):
-  game = n % (m+1)
-  if game == 0:
-    game = m
-  
-  print(f'\nO computador tirou {game} peça.')
+def computador_escolhe_jogada(n, m):
+  if n <= m:
+    return n
+  else:
+    move = n % (m+1)
+    if move > 0:
+      return move
+  return m
 
-  return n-game
 
-def user_game(n, m):
-  game = 0
+def usuario_escolhe_jogada(n, m):
+  move = 0
 
-  while game == 0:
-    game = int(input('\nQuantas peças você vai tirar? '))
-    if game <= 0 or game > m:
+  while move == 0:
+    move = int(input('\nQuantas peças você vai tirar? '))
+    if move > m or move <= 0:
       print('\nOops! Jogada inválida! Tente de novo.')
-      game = 0
+      move = 0
 
-  print(f'Você tirou {game} peça.')
-  return n-game
+  print(f'Você tirou {move} peça.')
+  return move
 
 
-def game_round(count):
-  print(f'**** Rodada {count} ****')
+def partida():
   n = int(input('Quantas peças? '))
   m = int(input('Limite de peças por jogada? '))
   
-  pc_start = check_multiple(n, m)
+  pc_start = n % (m+1)
 
   if pc_start:
     print('\nComputador começa!')
@@ -49,33 +49,41 @@ def game_round(count):
 
   while n > 0:
     if play_now:
-      n = pc_game(n, m)
+      move = computador_escolhe_jogada(n, m)
+      print(f'\nO computador tirou {move} peça.')
       play_now = False
     else:
-      n = user_game(n, m)
+      move = usuario_escolhe_jogada(n, m)
       play_now = True
     
+    n -= move
     if n == 0:
       print('Fim do jogo! O computador ganhou!')
     else:
       print(f'Agora restam {n} peças no tabuleiro.')
+    
+
+def compeonato():
+  round = 1
+  while round <= 3:
+    print(f'**** Rodada {round} ****')
+    partida()
+    round += 1
+  
+  print('\n**** Final do campeonato! ****')
+  print('\nPlacar: Você 0 X 3 Computador')
 
 
-
-def game_nim():
+if __name__ == '__main__':
   print(MSG_INIT)
+  choice = 0
+
   choice = int(input())
   if choice == 1:
     print('Você escolheu uma partida isolada')
-    game_round(1)
+    partida()
   elif choice == 2:
     count = 1
     print('Voce escolheu um campeonato!')
-    while count <= 3:
-      game_round(count)
-      count += 1
-    print('\n**** Final do campeonato! ****')
-    print('\nPlacar: Você 0 X 3 Computador')
+    compeonato()
 
-
-game_nim()
